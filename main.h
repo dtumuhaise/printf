31c8c59f8e3c6f37b718fbbb3f51b19ccef6d7e2
Devis <tumuhaise@gmail.com>
2022-08-09 05:54:52 -0700

    updated main.h

--- main.h
+++ main.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...);
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+
+
+
+int _printf(const char *format, ...);
 int _putchar(char c);
 
-char *convert(unsigned int num, int base);
 
 
 #endif
