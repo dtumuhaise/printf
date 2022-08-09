4d5179d7b41f4e8cd7004cf20d090d43a3212d87
dtumuhaise <44263038+dtumuhaise@users.noreply.github.com>
2022-08-09 22:46:57 +0300

    Update str_print.c

--- str_print.c
+++ str_print.c
@@ -1,4 +1,27 @@
 #include "main.h"
+
+/**
+ * func_revstr - function that reverses a string
+ * @list: is the list of arguments the function _printf receives
+ * Return: Number of caracteres printing
+ */
+int func_revstr(va_list list)
+{
+	char *str;
+	int count = 0, i, j = 0;
+
+	str = va_arg(list, char *);
+	for (i = 0; str[i] != 0; i++)
+	{
+		count++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(str[j]);
+	}
+	return (count);
+}
+
 /**
  * func_stringUppercase - Prints a string, when out of range print \x0A
  * @list: list of arguments the function _printf is sending
