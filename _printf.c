49163f9854b6f5434002ca25604d5eea25119341
Devis <tumuhaise@gmail.com>
2022-08-09 06:15:05 -0700

    updated main.h and _printf.c

---
+++ _printf.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _printf - print char or string
+ * @fomart: character string
+ * Return: number of characters being printed
+ */
+
+int _printf(const char *format, ...)
+{
+	st_fmt st_format[] = {
+		{"c", func_char},
+		{"s", func_string},
+		{"%", func_percent}'
+		{NULL, NULL}};
+
+	va_list list;
+	int count = 0;
+
+	va_start(list, format);
+	count = get_match_func(format, list, st_format);
+	va_end(list);
+	return (count);
+}
--- main.h
+++ main.h
@@ -8,11 +8,24 @@
 #include <limits.h>
 #include <stdio.h>
 
+/**
+ * struct format - structure
+ * @fmt: index
+ * @func: pointer to function
+ */
+typedef struct st_fmt
+{
+	char *fmt;
+	int (*func)(va_list list);
+} st_fmt;
 
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-
+int func_string(va_list);
+int func_char(va_list);
+int func_percent(va_list);
+int get_match_func(const char *, va_list, st_fmt st_format[]);
 
 
 #endif
