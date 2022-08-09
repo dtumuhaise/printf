7a23eb8b6eb0076c326fb1c17cc407fe751f765f
dtumuhaise <44263038+dtumuhaise@users.noreply.github.com>
2022-08-09 22:15:24 +0300

    Create conv_bin.c

---
+++ tests/conv_bin.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * bin_oct_hex_convert - convert a number into binary, octal or hexadecimal
+ * @num: Its the number given to convert
+ * @base: is the base which the number will be divided with; binary 2; octal 8; hexadecima 16.
+ *Return: The number of characteres printed
+ */	
+int bin_oct_hex_convert(unsigned int num, int base, int flag)
+{
+	char hexUpcase[] = "0123456789ABCDEF";
+	char hexLowcase[] = "0123456789abcdef";
+	char array[2001];
+	char *ptr;
+	int i = 2000, j = 0;
+
+	ptr = &array[2000];
+	*ptr = '\0';
+
+	do {
+		if (flag != 0)
+			*--ptr = hexUpcase[num % base];
+		else
+			*--ptr = hexLowcase[num % base];
+		num = num / base;
+		i--;
+	} while (num != 0);
+
+	for (j = i; array[j] != 0; j++)
+	{
+		_putchar(array[j]);
+	}
+	return(2000 - i);
+}
